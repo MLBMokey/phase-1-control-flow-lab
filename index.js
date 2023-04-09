8085b73fb153d36d540d677b92f7887042542239
Anthony Lopez <anthonydlr66@gmail.com>
2023-04-08 22:32:57 -0400

    strictly >

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number) {
+  if (number <= 400) {
+      return 'This one is on me!'
+  } else if (number < 2000 ) {
+      return 'That will be twenty bucks.'
+  } else if (number < 2500) {
+      return 'I will gladly take your thirty bucks.'
+  } else if (number > 2500) {
+      return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(string) {
+ if (string === 'NYC') {
+  return 'Ok, sounds good.'
+ } else {
+  return 'No go.'
+ }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(string) {
+  switch(string) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous': 
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+ 
 }
